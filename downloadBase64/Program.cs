4a5128e03bf6f77a379934017619b95a7defd9fe
Quentin Brooks <quentin.brooks@zazzle.com>
2015-09-20 01:35:25 -0400

    Some caching, to make it faster.

--- downloadBase64/Program.cs
+++ downloadBase64/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -13,11 +14,33 @@ namespace downloadBase64
         {
             string url = args.Length > 0 ? args[0] : "http://www.google.com/favicon.ico";
 
-            byte[] image = new WebClient().DownloadData(url);
+            string fileName = url.GetHashCode() + ".dataurl.cache";
+            string urlMarker = fileName + ".marker.cache";
 
-            string dataUrl = Convert.ToBase64String(image);
+            if (File.Exists(urlMarker) && File.ReadAllText(urlMarker) == url)
+            {
+                Console.Write(File.ReadAllText(fileName));
+            }
+            else
+            {
+                string dataUrl;
+                try
+                {
+                    byte[] image = new WebClient().DownloadData(url);
+                    dataUrl = Convert.ToBase64String(image);
+                }
+                catch (Exception e)
+                {
+                    //Cache the error! Because if the favicon.ico is dead, I don't want to waste time verifying it...
+                    dataUrl = e.ToString();
+                }
 
-            Console.WriteLine(dataUrl);
+                //TODO: Do some cache eviction... for now... well they are just favicons, so it shouldn't matter.
+                File.WriteAllText(fileName, dataUrl);
+                File.WriteAllText(urlMarker, url);
+
+                Console.Write(dataUrl);
+            }
         }
     }
 }
